12ec7fa39816752870e6fb22776c9b1470127455
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-04-12 14:19:26 -0500

    Initial Phaser with webpack template test file

--- src/index.js
+++ src/index.js
@@ -1,49 +1,34 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-import 'phaser';
+import Phaser from 'phaser';
+import './style/style.scss';
+import logoImg from './assets/logo.png';
 
 function preload() {
-  this.load.setBaseURL('http://labs.phaser.io');
-
-  this.load.image('sky', 'assets/skies/space3.png');
-  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-  this.load.image('red', 'assets/particles/red.png');
+  this.load.image('logo', logoImg);
 }
 
 function create() {
-  this.add.image(400, 300, 'sky');
+  const logo = this.add.image(400, 150, 'logo');
 
-  const particles = this.add.particles('red');
-
-  const emitter = particles.createEmitter({
-    speed: 100,
-    scale: { start: 1, end: 0 },
-    blendMode: 'ADD',
+  this.tweens.add({
+    targets: logo,
+    y: 450,
+    duration: 2000,
+    ease: 'Power2',
+    yoyo: true,
+    loop: -1,
   });
-
-  const logo = this.physics.add.image(400, 100, 'logo');
-
-  logo.setVelocity(100, 200);
-  logo.setBounce(1, 1);
-  logo.setCollideWorldBounds(true);
-
-  emitter.startFollow(logo);
 }
 
 const config = {
   type: Phaser.AUTO,
+  parent: 'phaser-example',
   width: 800,
   height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 },
-    },
-  },
   scene: {
     preload,
     create,
   },
 };
 
+// eslint-disable-next-line no-unused-vars
 const game = new Phaser.Game(config);
