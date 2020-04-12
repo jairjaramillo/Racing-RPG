b4f38250a28e54f43bc217f3d0f0746dd08d0678
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-04-12 17:58:28 -0500

    Add initial code

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,14 +1,12 @@
-import 'phaser';
+import Phaser from 'phaser';
 import logo from '../assets/logo.png';
 
-// eslint-disable-next-line no-undef
 export default class GameScene extends Phaser.Scene {
   constructor() {
     super('Game');
   }
 
   preload() {
-    // load images
     this.load.image('logo', logo);
   }
 
